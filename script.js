4c33c11164b936f3790287586d2522aa31dfa158
Basavaraj Dharwad <Basavaraj.cd@relyonsoft.com>
2025-07-22 10:46:34 +0530

    Add portfolio files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+//   console.log(e);
+    alert("Thanks for contacting me! I'll get back to you soon.");
+});
